af084ece7c83152e3df38a8f4b9c559f560d3858
Sebastian Seilund <sebastian@seilund.com>
2013-03-01 20:15:57 -0800

    Added success and error methods to ModelOperationPromise

--- src/model-operation-promise.js
+++ src/model-operation-promise.js
@@ -3,6 +3,12 @@ BD.ModelOperationPromise = Em.Object.extend(Em.Evented, {
     on: function() {
         this._super.apply(this, arguments);
         return this;
+    },
+    success: function(callback) {
+        return this.on('success', callback);
+    },
+    error: function(callback) {
+        return this.on('error', callback);
     }
 
 });
