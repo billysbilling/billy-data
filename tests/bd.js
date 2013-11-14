86e4239aba05bbfcf89b0dd8a5a7eacc3d47c41d
Michael Storgaard <github@michaelstorgaard.com>
2013-11-13 17:15:04 -0800

    Moved inflectors to external module
     - Moved pluralize, singularize, and classify to inflectors module
     - Added adding/deleting of plural rules
     - Added tests for classify and adding/deleting of plural rules

--- tests/bd.js
+++ tests/bd.js
@@ -19,3 +19,20 @@ test('Singularizer', function() {
     equal(BD.singularize('batches'), 'batch');
     equal(BD.singularize('bankLineMatches'), 'bankLineMatch');
 });
+
+test('Inflector rules', function() {
+    BD.addInflectorsRule('foo', 'bar');
+    equal(BD.pluralize('foo'), 'bar');
+    equal(BD.singularize('bar'), 'foo');
+    BD.removeInflectorsRule('foo');
+
+    BD.addInflectorsRule('foo2', 'bars');
+    BD.removeInflectorsRule('foo2');
+    equal(BD.pluralize('foo'), 'foos');
+    equal(BD.singularize('bars'), 'bar');
+});
+
+test('Classify', function() {
+    equal(BD.classify('invoice'), 'Invoice');
+    equal(BD.classify('bankLineMatch'), 'BankLineMatch');
+});
