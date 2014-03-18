f174bfb06bfe6a9d77ec4c892648a0853bccd3c5
Sebastian Seilund <sebastian@seilund.com>
2014-03-18 16:08:24 -0700

    Removed FixtureAdapter Use billy-data-fixture-adapter instead

--- src/js/adapters.js
+++ src/js/adapters.js
@@ -1,3 +1,1 @@
-require('./adapters/fixture-adapter');
-require('./adapters/fixture-request');
 require('./adapters/rest-adapter');
--- tests/store/store-adapter.js
+++ tests/store/store-adapter.js
@@ -1,12 +1,18 @@
 QUnit.module('BD.Store adapter features');
 
+var TestAdapter = Em.Object.extend({
+    toString: function() {
+        return 'TestAdapter';
+    }
+});
+
 test('should have default adapter of BD.RestAdapter', function() {
     ok(BD.store.get('adapter').toString().match(/BD.RestAdapter/));
 });
 
 test('should be able to set the adapter', function() {
     var oldAdapter = BD.store.get('adapter');
-    BD.store.set('adapter', BD.FixtureAdapter.create({}));
-    ok(BD.store.get('adapter').toString().match(/BD.FixtureAdapter/));
+    BD.store.set('adapter', TestAdapter.create({}));
+    ok(BD.store.get('adapter').toString().match(/TestAdapter/));
     BD.store.set('adapter', oldAdapter);
 });
